f0bbcba62a4fc9b4edfab476a4f948b3bbfee649
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-11-14 11:08:19 +0100

    chore: add pages and paths to enigmas

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Menu from '../views/Menu.vue';
+import Enigma1 from '../views/Enigmas/Enigma1.vue';
+import Enigma2 from '../views/Enigmas/Enigma2.vue';
+import Enigma3 from '../views/Enigmas/Enigma3.vue';
+import Enigma4 from '../views/Enigmas/Enigma4.vue';
+import Enigma5 from '../views/Enigmas/Enigma5.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/menu', component: Menu },
+  { path: '/enigma1', component: Enigma1 },
 ];
 
 const router = createRouter({
---
+++ src/views/Enigmas/Enigma3.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Enigma 1</h1>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style></template>
